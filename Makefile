46809f2148e69b3de4a34b10e1a97884048e1045
kanhar <knaan.harpaz@gmail.com>
2019-06-16 16:50:05 -0400

    Now working insertion

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ CPPFLAGS=-O2 -std=c++17 -Wall
 main: main.o
 	g++ -o main main.o
 
-main.o: main.cpp RBNode.h
+main.o: main.cpp RBTree.h
 	g++ $(CPPFLAGS) -c main.cpp
 
 clean:
--- main.cpp
+++ main.cpp
@@ -2,26 +2,29 @@
 #include <iostream>
 #include <string>
 
-#include "RBNode.h"
+#include "RBTree.h"
 
 using std::cout, std::endl, std::string;
 
 /*
- * @return: positive integer if str1 comes before str2 in lexicographical order
+ * @return: negative integer if str1 comes before str2 in lexicographical order
  *          0 if equal
- *          negative integer if str2 comes before str1
+ *          positive integer if str2 comes before str1
  */
 int strings_cmp_callback(const string& str1,const string& str2) {
-    return str2.compare(str1);
+    return str1.compare(str2);
 }
 
 int main(int argc, char** argv) {
     cout << "Creating tree" << endl;
-    auto tree = RBNode<string>::createRBTree(strings_cmp_callback);
-    cout << "Tree created, inserting 'BBB'" << endl;
+    auto tree = RBTree<string>::createTree(strings_cmp_callback);
+    tree->insert("BBA");
     tree->insert("BBB");
-    cout << "inserting 'BBC'" << endl;
     tree->insert("BBC");
-    cout << "inserting 'BBD'" << endl;
     tree->insert("BBD");
+    tree->insert("BBE");
+    tree->insert("BBF");
+    tree->insert("BBG");
+    tree->insert("BBH");
+    cout << "All good" << endl;
 }
